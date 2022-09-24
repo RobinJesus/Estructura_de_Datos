b5d93333f95f270e90bce1cccb91b9d89ba23033
Robin1594 <113863739+RobinJesus@users.noreply.github.com>
2022-09-23 21:03:24 -0600

    Create Recursion.java

---
+++ src/main/java/TrabajoClase_2/Recursion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TrabajoClase_2;
+
+/**
+ *
+ * @author rjesu
+ */
+public class Recursion {
+    
+    
+
+}
+
+public static int factorial(int n){
+
+if()
+
+}
