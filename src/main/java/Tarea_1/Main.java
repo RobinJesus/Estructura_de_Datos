707a4a4cce5e260dafc2593a334ae6ca727252ba
Robin1594 <113863739+RobinJesus@users.noreply.github.com>
2022-09-30 00:35:04 -0600

    Prueba

---
+++ src/main/java/Tarea_1/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Tarea_1;
+
+/**
+ *
+ * @author rjesu
+ */
+public class Main {
+    
+    
+
+    public static void  CrearVector(String[] args) {
+        
+        System.out.print("Digite el tamaño ");
+         int tamaño = new java.util.Scanner(System.in).nextInt();
+         int vector []= new int [tamaño];
+         
+         System.out.println(Vector.devolver(vector, tamaño));
+         System.out.println(Vector.valorMay(vector, tamaño, tamaño));
+    }
+}
